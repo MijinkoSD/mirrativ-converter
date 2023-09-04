a8479f9d822fa3b0a156037f2fa26c51bd804d22
Mijinko_SD <79152329+MijinkoSD@users.noreply.github.com>
2023-09-04 20:55:37 +0900

    Init: 初回コミット

---
+++ run.ps1
@@ -0,0 +1,8 @@
+.\scripts\active-venv.ps1
+
+# この下で任意のファイルの実行をする
+python .\sample\hello.py # Hello World!
+
+
+# 終了時（任意）
+deactivate
---
+++ test/test_dummy.py
@@ -0,0 +1,17 @@
+"""ダミーのテストコード
+"""
+
+import unittest
+
+
+class TestDummy(unittest.TestCase):
+    """ダミー
+
+    Args:
+        unittest (_type_): _description_
+    """
+
+    def test_dummy(self) -> None:
+        """ダミー
+        """
+        pass
